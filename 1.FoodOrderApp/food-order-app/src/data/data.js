d44cf079cc8d03c43d1b0bf2e626d6cc7a755a67
Spronks <ntassop@yahoo.com>
2022-01-19 22:14:35 +0200

    commit menu page

---
+++ 1.FoodOrderApp/food-order-app/src/data/data.js
@@ -0,0 +1,39 @@
+import Chole from "../images/chhola.jpg";
+import Dosa from "../images/dosa.jpg";
+import Paneer from "../images/paneer.jpg";
+import Idli from "../images/idli.jpg";
+import Gujrati from "../images/gujrati.jpeg";
+import Rajasthani from "../images/rajasthani.jpg";
+
+export const Data = [
+  {
+    name: "Chole Bhature",
+    image: Chole,
+    price: 2.5,
+  },
+  {
+    name: "Masala Dosa",
+    image: Dosa,
+    price: 2.8,
+  },
+  {
+    name: "Butter Paneer",
+    image: Paneer,
+    price: 3.5,
+  },
+  {
+    name: "Idli Vada",
+    image: Idli,
+    price: 2.3,
+  },
+  {
+    name: "Gujrati Thali",
+    image: Gujrati,
+    price: 4.5,
+  },
+  {
+    name: "Rajasthani Thali",
+    image: Rajasthani,
+    price: 5.5,
+  },
+];
