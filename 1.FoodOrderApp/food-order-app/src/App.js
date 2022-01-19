ea5e0836a04fa91f0683e62fbe516b1e8762f25b
Spronks <ntassop@yahoo.com>
2022-01-19 18:40:24 +0200

    upload navbar

--- 1.FoodOrderApp/food-order-app/src/App.js
+++ 1.FoodOrderApp/food-order-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Router>
+        <Navbar />
+      </Router>
     </div>
   );
 }
