0ca8b253015579ad710af8e5e45e747c9a20fed9
Spronks <ntassop@yahoo.com>
2022-01-19 20:26:30 +0200

    commit homepage and footer

--- 1.FoodOrderApp/food-order-app/src/App.js
+++ 1.FoodOrderApp/food-order-app/src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+        <Footer />
       </Router>
     </div>
   );
