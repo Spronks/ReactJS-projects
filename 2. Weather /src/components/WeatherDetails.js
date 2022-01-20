fb47bda2fdbd0b8e98f01346c0bbde4f3e43e6cd
Spronks <ntassop@yahoo.com>
2022-01-20 23:00:29 +0200

    style weather card with dummy data

--- 2. Weather /src/components/WeatherDetails.js
+++ 2. Weather /src/components/WeatherDetails.js
@@ -7,13 +7,62 @@ function WeatherDetails() {
         <div className="weatherIcon">
           <i className="wi wi-day-sunny"></i>
         </div>
+
         <div className="weatherInfo">
           <div className="temperature">
             <span>36&deg;C</span>
           </div>
+
           <div className="description">
             <div className="weatherCondition">sunny</div>
-            <div className="plsv">Athens, Greece</div>
+            <div className="place">Athens, Greece</div>
+          </div>
+        </div>
+        <div className="date">{new Date().toLocaleString()}</div>
+
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                8:00 PM <br />
+                Sunset
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                60% <br />
+                Humidity
+              </p>
+            </div>
+          </div>
+
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-rain"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                42mm <br />
+                Pressure
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-strong-wind"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                8 beaufort <br />
+                Wind
+              </p>
+            </div>
           </div>
         </div>
       </article>
