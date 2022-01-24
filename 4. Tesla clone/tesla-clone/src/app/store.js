4c831c5756c8887c510237d332f74ec254ff7b24
Spronks <ntassop@yahoo.com>
2022-01-24 04:01:18 +0200

    add redux for cars

---
+++ 4. Tesla clone/tesla-clone/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import carReducer from "../features/car/carSlice";
+
+export default configureStore({
+  reducer: {
+    car: carReducer,
+  },
+});
